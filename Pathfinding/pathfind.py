900ecedd902ea418dddabe8826e6e83cccc51c7a
zhuweiji <55840431+zhuweiji@users.noreply.github.com>
2022-02-18 09:00:08 +0800

    changed all pathfind functions to take final facing direction into account

--- Pathfinding/pathfind.py
+++ Pathfinding/pathfind.py
@@ -62,7 +62,7 @@ class Pathfinder:
     ARENA_SIZE = (19,19)
 
     @classmethod
-    def get_path_between_points(cls, node_list: typing.List[Node], facing_direction_for_node_list: typing.List[str], obstacles: list, update_callback=None):
+    def get_path_between_points(cls, node_list: typing.List[Node], obstacles: list, update_callback=None, facing_direction_for_node_list: typing.List[str]=[]):
         '''	
         Returns a path using the internal Pathfinder get_path function that will travel between the given nodes, in the order they were given.
         
@@ -84,22 +84,24 @@ class Pathfinder:
         '''
 
         total_path, total_distance = [], 0
-        if len(facing_direction_for_node_list) == len(node_list):
-            raise ValueError('List of facing directions should be one less than the list of target nodes, because there is no target direction at the first node')
+        if facing_direction_for_node_list:
+            if len(facing_direction_for_node_list) == len(node_list):
+                raise ValueError('List of facing directions should be one less than the list of target nodes, because there is no target direction at the first node')
         
         facing_direction_at_end = None
+
         for i in range(len(node_list)-1):
             start_node, end_node = node_list[i], node_list[i+1]
             if facing_direction_for_node_list:
                 facing_direction_at_end = facing_direction_for_node_list[i]
-            res = Pathfinder.get_path_between_two_points(start_node, end_node, obstacles, facing_direction_at_end, update_callback)
+            res = Pathfinder.get_path_between_two_points(start=start_node, target=end_node, obstacles=obstacles, update_callback=update_callback, direction_at_target=facing_direction_at_end)
             total_path.append(res['path'])
             total_distance += res['distance']
 
         return {'path': total_path, 'distance': total_distance}
 
     @classmethod
-    def shortest_path_to_n_points(cls, start_node: Node, node_list: typing.List[Node], facing_direction_for_node_list: typing.List[str], obstacles: list, approximate=True, update_callback=None):
+    def shortest_path_to_n_points(cls, start_node: Node, node_list: typing.List[Node], obstacles: list, approximate=True, update_callback=None, facing_direction_for_node_list: typing.List[str]=[]):
         '''	
         Given a unordered list of nodes, and a starting location, try to find the shortest path that will travel to all of the given nodes
         
@@ -128,9 +130,8 @@ class Pathfinder:
             temp_result = []
             all_paths = list(itertools.permutations(node_list))
             all_paths = [(start_node, *path) for path in all_paths]
-
             for path in all_paths:
-                _ = cls.get_path_between_points(path, facing_direction_for_node_list, obstacles, update_callback)
+                _ = cls.get_path_between_points(node_list=path, obstacles=obstacles, update_callback=update_callback, facing_direction_for_node_list=facing_direction_for_node_list)
                 temp_result.append((_['path'], _['distance']))
 
             path, distance = sorted(temp_result, key=lambda x: x[1])[0]
@@ -145,14 +146,13 @@ class Pathfinder:
             while node_list:
                 closest_node = sorted(node_list, key=lambda node:Pathfinder.h_function(*current_node, *node))[0]
                 traversal_order.append(closest_node)
-                facing_direction_in_traversal_order.append(facing_direction_for_node_list[node_list.index(closest_node)])
+                if facing_direction_for_node_list:
+                    facing_direction_in_traversal_order.append(facing_direction_for_node_list[node_list.index(closest_node)])
                 node_list.remove(closest_node)
                 current_node = closest_node
 
             print(f'{traversal_order=}')
-            return cls.get_path_between_points(traversal_order, facing_direction_in_traversal_order, obstacles, update_callback)
-
-                    
+            return cls.get_path_between_points(node_list=traversal_order, obstacles=obstacles, update_callback=update_callback, facing_direction_for_node_list=facing_direction_for_node_list)
 
             # remaining_nodes = node_list[:]
             # closest_node, shortest_path, shortest_distance = None, [], float('inf')
@@ -167,7 +167,7 @@ class Pathfinder:
 
     @classmethod
     def get_path_between_two_points(cls, start: Node, target: Node, obstacles: list,
-        direction_at_target=None, update_callback=None, standoff_distance=None):
+        direction_at_target=None, update_callback=None, standoff_distance=0):
         '''	
         Use A* to calculate a path between two points
         
@@ -178,6 +178,8 @@ class Pathfinder:
         obstacles: List(node) or List(tuple x,y)
         direction_at_target: str Left, Right, Up, Down
             direction the agent should be facing at the target point
+            Using this argument breaks the speed of A* since the costing function can no longer accurately determine the cost to the target
+            because the optimal path might involve moving away from the target to orient itself to the right facing direction
         update_callback: function
             logging function to be called when path has been found
         standoff_distance: int
@@ -227,7 +229,7 @@ class Pathfinder:
         assert cls.ROBOT_SIZE == (3,3)
         assert cls.OBSTACLE_SIZE == (1,1)
 
-        while True:
+        while queue:
             cost, (current_x, current_y), current_direction, path_to_current = heapq.heappop(queue)
             memo_key = (current_x, current_y)
 
@@ -255,14 +257,19 @@ class Pathfinder:
                             update_callback(f'{next_direction}')
                         return {'path': path_to_next, 'distance': next_cost}
 
-                heapq.heappush(queue, (next_cost, (x, y),next_direction, path_to_next))
+                heapq.heappush(queue, (next_cost, (x, y), next_direction, path_to_next))
 
-            if abs(current_x - ex) <= standoff_distance or abs(current_y - ey) <= standoff_distance:
-                pass
+            if standoff_distance:
+                if abs(current_x - ex) <= standoff_distance or abs(current_y - ey) <= standoff_distance:
+                    pass
+                else:
+                    visited[memo_key] = True
             else:
                 visited[memo_key] = True
 
             logger()
+        return {'path': [], 'distance': float('inf')}
+
 
     @classmethod
     def points_are_out_of_bounds(cls, x,y):
--- Pathfinding/test_pathfind.py
+++ Pathfinding/test_pathfind.py
@@ -94,9 +94,9 @@ if __name__ == "__main__":
     print(f'{obstacles=}')
 
     facing_directions = Pathfinder.find_facing_directions(targets, obstacles)
-    res = Pathfinder.shortest_path_to_n_points(
-        start, targets, facing_direction_for_node_list=facing_directions, obstacles=obstacles, update_callback=points_completed_updater)
-
+    # res = Pathfinder.shortest_path_to_n_points(
+        # start, targets, facing_direction_for_node_list=facing_directions, obstacles=obstacles, update_callback=points_completed_updater)
+    res = Pathfinder.shortest_path_to_n_points(start, targets, obstacles=obstacles, update_callback=points_completed_updater, facing_direction_for_node_list=[])
     path = Pathfinder.flatten_output(res['path'])
 
     print(f'\n{path=}\n')
