127d9312905826dfcb35c32134797d37a37e5442
Aru <rm.arumugam.2000@gmail.com>
2022-03-01 10:30:36 +0800

    Some rpi code

--- mdp_comm/obstacle_storage.py
+++ mdp_comm/obstacle_storage.py
@@ -5,7 +5,7 @@ class Obstacle:
     x: int
     y: int
     obstacle_id: int
-    obstacle_direction: str
+    direction: str
 
 class ObstacleStorage:
 
@@ -13,7 +13,14 @@ class ObstacleStorage:
         self._obstacles = {}
 
     def update_obstacle(self, obstacle_id, obstacle_direction, x, y):
-        self._obstacles[obstacle_id] = Obstacle(obstacle_id=obstacle_id, obstacle_direction=obstacle_direction, x=x, y=y)
+        dir_map = {
+            "up":"N",
+            "down": "S",
+            "left":"W",
+            "right":"E",
+            "null":"null"
+        }
+        self._obstacles[obstacle_id] = Obstacle(obstacle_id=obstacle_id, direction=dir_map[obstacle_direction], x=x, y=y)
 
     def extract_required_format(self):
-        return [obs.obstacle_direction for obs in self._obstacles.values()], [(obs.x, obs.y) for obs in self._obstacles.values()]
+        return [obs.direction for obs in self._obstacles.values() if obs.direction!="null"], [(obs.x * 10, obs.y * 10) for obs in self._obstacles.values() if obs.direction != "null"], [(obs.x * 10, obs.y * 10) for obs in self._obstacles.values() if obs.direction == "null"]
